7da874d91ab82173be05232866795d3af468ee57
Lalit R Nagpal <lalit2801@gmail.com>
2021-02-14 17:38:57 +0530

    Flexible Lists

---
+++ 15-flexible-lists/src/Person/Person.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Person.css';
+
+const Person = ( props ) => {
+   return (          
+      <div class="Person">
+         <p onClick={props.click}>I'm a Person named {props.name} who is {props.age} years old!</p>
+         <p>The content passed is: {props.children}</p>
+         <input type="text" onChange={props.changed} value={props.name}/>
+      </div>
+   )
+};
+
+export default Person;
