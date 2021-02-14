99234bc7663cf571993bb9b3c2843ab064f4c2f0
Lalit R Nagpal <lalit2801@gmail.com>
2021-02-14 17:43:10 +0530

    More on Styled Components

---
+++ 21-more-on-styled-components/src/Person/Person.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './Person.css';
+import styled from 'styled-components';
+
+const StyledDiv  = styled.div`
+   width: 60%;
+   margin: auto;
+   border: 4px solid #eee;
+   box-shadow: 0 4px 5px #ccc;
+   background-color:MistyRose;
+   padding: 16px;
+   text-align: center;
+`
+
+const Person = ( props ) => {
+   return (          
+      <StyledDiv>
+         <p onClick={props.click}>I'm a Person named {props.name} who is {props.age} years old!</p>
+         <p>The content passed is: {props.children}</p>
+         <input type="text" onChange={props.changed} value={props.name}/>
+      </StyledDiv>
+   )
+};
+
+export default Person;
