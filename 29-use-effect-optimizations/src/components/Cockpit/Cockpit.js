626253b51d8892d9abc5df486bc3eeb49fab3607
Lalit R Nagpal <lalit2801@gmail.com>
2021-02-15 15:48:55 +0530

    Use Effect Optimizations

---
+++ 29-use-effect-optimizations/src/components/Cockpit/Cockpit.js
@@ -0,0 +1,60 @@
+import React, { useEffect } from 'react';
+
+import classes from './Cockpit.css';
+
+const cockpit = ( props ) => {
+
+    useEffect( () => {
+        console.log('[Cockpit.js] -> useEffect called');
+        const timer = setTimeout(() => {
+          alert('Saved data to the cloud');
+        }, 1000);
+
+        return () => {
+          clearTimeout(timer);
+          console.log('[Cockpit.js] cleanup work in useEffect');
+        };
+    }, [props.persons] );
+
+    useEffect( () => {
+      console.log('[Cockpit.js] -> useEffect called');
+      setTimeout(() => {
+        alert('Running only the first time');
+      }, 1000);
+    }, [] );
+    // useEffect
+
+    useEffect(() => {
+      console.log('[Cockpit.js] 2nd useEffect');
+      return () => {
+        console.log('[Cockpit.js] cleanup work in 2nd useEffect');
+      };
+    });
+  
+    // useEffect();
+
+    const assignedClasses = [];
+    let btnClass = '';
+    if (props.showPersons) {
+        btnClass = classes.Red;
+    }
+
+    if ( props.persons.length <= 2 ) {
+      assignedClasses.push( classes.red ); // classes = ['red']
+    }
+    if ( props.persons.length <= 1 ) {
+      assignedClasses.push( classes.bold ); // classes = ['red', 'bold']
+    }
+
+    return (
+        <div className='App'>
+            <h1>{props.title}</h1>
+            <p className={assignedClasses.join( ' ' )}>This is really working!</p>
+            <button
+                className={btnClass}
+                onClick={props.clicked}>Toggle Persons</button>
+        </div>
+    );
+};
+
+export default cockpit;
