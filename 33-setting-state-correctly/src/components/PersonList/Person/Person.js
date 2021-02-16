dff16c01373e5ba7ba54d21c4ab96fd6dccac8c1
Lalit R Nagpal <lalit2801@gmail.com>
2021-02-16 11:28:54 +0530

    Setting State correctly

---
+++ 33-setting-state-correctly/src/components/PersonList/Person/Person.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Auxilliary from '../../hoc/Auxilliary';
+import withClass from '../../hoc/withClass';
+import './Person.css';
+
+class Person extends Component {
+  render() {
+    console.log('[Person.js] rendering...');
+    return (
+      <Auxilliary>
+        <p onClick={this.props.click}>
+          I'm {this.props.name} and I am {this.props.age} years old!
+        </p>
+        <p key='i2' >{this.props.children}</p>
+        <input key='i3' 
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </Auxilliary>
+    );
+  }
+}
+
+export default withClass(Person, 'Person');
