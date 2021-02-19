ee693536bf9b033d5ffa6b656231b37f2b4855fd
Lalit R Nagpal <lalit2801@gmail.com>
2021-02-19 13:30:05 +0530

    http interceptors

--- 47-http-interceptors/src/axios.js
+++ 47-http-interceptors/src/axios.js
@@ -4,6 +4,15 @@ const instance = axios.create({
     baseURL: 'https://jsonplaceholder.typicode.com'
 })
 
+instance.interceptors.request.use(request => { 
+    console.log('request via interceptor ');
+    console.log(request);
+    return request;
+}, error => {
+    console.log('error caught by interceptor');
+    return Promise.reject(error);
+});
+
 instance.defaults.headers.common['Authorization'] = 'AUTH TOKEN FROM INSTANCE';
 instance.defaults.headers.post['Content-Type'] = 'application/json';
 
