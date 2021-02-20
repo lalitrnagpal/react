4af916f091f9d8f1b2e6d17553b44c3b6ac80abe
Lalit R Nagpal <lalit2801@gmail.com>
2021-02-20 10:31:40 +0530

    Parameters, Navigation, Programmatic Navigation

---
+++ 51-routing-params-navigation/src/containers/Blog/Blog.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+// import axios from 'axios';
+import axios from '../../axios';
+import Posts from './Posts/Posts';
+import { Route, NavLink, Switch } from 'react-router-dom';
+import classes from './Blog.css';
+import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
+
+class Blog extends Component {
+
+    render () {
+        return (
+            <div className='Blog'>
+                <header>
+                    <nav>
+                        <ul>
+                            <li><NavLink 
+                                exact 
+                                to='/'
+                                /* activeClassName='my-active' */
+                                activeClassName='active'
+                                activeStyle={{
+                                    color: 'dodgerblue',
+                                    textDecoration: 'underline'
+                                 }}          
+                                >Home</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post', 
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</NavLink></li>
+                        </ul>
+                    </nav>
+                </header>
+                { /* <Route path="/" exact render={() => <h1>Home</h1>}/>
+                <Route path="/" render={() => <h1>Home 2</h1>}/> */ }
+                <Switch>
+                    <Route path="/" exact component={Posts}/>
+                    <Route path="/new-post" component={NewPost}/>
+                    <Route path="/:id" exact component={FullPost}/>
+                </Switch>
+
+            </div>
+        );
+    }
+}
+
+export default Blog;
