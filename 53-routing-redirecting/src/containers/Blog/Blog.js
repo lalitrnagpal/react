eff1c9d75f6ed8ce4211f18f7b8f8ff3be533b56
Lalit R Nagpal <lalit2801@gmail.com>
2021-02-20 14:21:08 +0530

    Routing Redirection

---
+++ 53-routing-redirecting/src/containers/Blog/Blog.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+// import axios from 'axios';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
+
+import './Blog.css';
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+
+class Blog extends Component {
+
+    state = {
+        auth: false
+    }
+
+    render () {
+        return (
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><NavLink
+                                to="/posts/"
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Posts</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</NavLink></li>
+                        </ul>
+                    </nav>
+                </header>
+                <Switch>
+                    { this.state.auth ? <Route path="/new-post" component={NewPost} /> : null }
+                    <Route path="/posts" component={Posts} />
+                    <Route render={() => <h1>Not found</h1>}/>
+                    {/* <Redirect from="/" to={Posts} /> */} 
+                </Switch>
+            </div>
+        );
+    }
+}
+
+export default Blog;
